0e765a4e0259e6e6ee53d039de3e9b071926067d
tr363 <tarun_01@yahoo.com>
2023-08-12 22:38:21 -0600

    intial push

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {About, Education, Skills, Projects, Experience, Contact} from './Components';
+
+function App() {
+  return (
+    <div className='App'>
+      <About />
+      <Education />
+      <Skills />
+      <Projects />
+      <Experience />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/About/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About(){
+    return(
+        <section id="about">
+            <div>
+            <h1>Hello, I'm Tarun</h1>
+            <h4>Master of Science in Computer Science student at University of Colorado Denver</h4>
+            <p>I work on projects related to Machine Learning, Artificial Intelligence, Computer Vision and Data Science.</p>
+        </div>
+        </section>
+    );
+}
+
+export default About;
